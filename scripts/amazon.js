740d850cc18b80edb728e2f2635be91227704eea
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-10-22 15:25:00 +0000

    Added a extra line of space below cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -83,6 +83,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
         document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
         console.log(cartQuantity);
         console.log(cart);
+        console.log(" ");
     })
 })
 
