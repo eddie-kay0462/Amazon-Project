dd473dcb1a82c5047e4d5ca229c1e58b846f3cbf
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-10-23 04:10:34 +0000

    save cart in local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,21 @@
-export let cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-},
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart){
+    cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+    },
+    {
+        productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+    }]
+}
+
+
+function saveToStorage()
 {
-    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
-}];
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 // add to cart
 export function addToCart(productId, value){
@@ -25,6 +35,7 @@ export function addToCart(productId, value){
             quantity:value
         })
     }
+    saveToStorage();
 }
 
 
@@ -37,4 +48,5 @@ export function removeFromCart(productId){
         }
     });
     cart = newCart;
+    saveToStorage();
 }
